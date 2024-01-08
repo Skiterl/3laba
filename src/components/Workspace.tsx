443904cf4030eb5288259a00301b7b409bba2233
Alex Zharkov <alexzhar03@gmail.com>
2024-01-09 00:38:14 +0300

    code: EBASHU

--- src/components/Workspace.tsx
+++ src/components/Workspace.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Matrix, Mxy, Mxz, Myz } from "../utils/math";
+import { Matrix } from "../utils/math";
 import Canvas3D from "./UI/Canvas3D/Canvas3D";
 
 interface WorkspaceInfo {
@@ -28,26 +28,25 @@ function Workspace({ k1, k2, k3 }: WorkspaceInfo) {
   const phiy =
     sinwz / coswy <= 1 ? Math.acos(sinwz / coswy) : Math.acosh(sinwz / coswy);
 
-  const sin_phix = Math.sin(phix);
-  const sin_phiy = Math.sin(phiy);
-
-  const sin_phiz = Math.sqrt(1 - sin_phix * sin_phix - sin_phiy * sin_phiy);
-  const phiz = Math.asin(sin_phiz);
   //-35.264389682754654 45 24.0948425521107
-  console.log(
-    (phix * 180) / Math.PI,
-    (phiy * 180) / Math.PI,
-    (phiz * 180) / Math.PI
-  );
 
   const _phix = (35.264389682754654 * Math.PI) / 180;
   const _phiy = (-45 * Math.PI) / 180;
-  const _phiz = (-24.0948425521107 * Math.PI) / 180;
+  const _phiz = (-35.264389682754654 * Math.PI) / 180;
 
   const _Rphix = new Matrix(4, 4, [
     [1, 0, 0, 0],
     [0, Math.cos(_phix), Math.sin(_phix), 0],
     [0, -Math.sin(_phix), Math.cos(_phix), 0],
+    [0, 0, 0, 1],
+  ]);
+
+  const angle = phix - 0.335;
+
+  const RphixToz = new Matrix(4, 4, [
+    [1, 0, 0, 0],
+    [0, Math.cos(angle), Math.sin(angle), 0],
+    [0, -Math.sin(angle), Math.cos(angle), 0],
     [0, 0, 0, 1],
   ]);
 
@@ -78,31 +77,142 @@ function Workspace({ k1, k2, k3 }: WorkspaceInfo) {
     [0, 0, 0, 1],
   ]);
 
-  const Rphiz = new Matrix(4, 4, [
-    [Math.cos(phiz), Math.sin(phiz), 0, 0],
-    [-Math.sin(phiz), Math.cos(phiz), 0, 0],
-    [0, 0, 1, 0],
-    [0, 0, 0, 1],
-  ]);
-
   const R = Rphix.multiply(Rphiy);
-  const Rxy = R.multiply(Mxy);
-  const Rxz = R.multiply(Mxz);
-  const Ryz = R.multiply(Myz);
 
   const rotateMatrixXY = R.multiply(_Rphiy).multiply(_Rphix);
-  const rotateMatrixXZ = R.multiply(_Rphiz).multiply(_Rphix);
-  const rotateMatrixYZ = R.multiply(_Rphiz).multiply(_Rphiy);
+  const rotateMatrixXZ = R.multiply(_Rphiy).multiply(RphixToz);
+  const rotateMatrixYZ = R.multiply(Rphiy).multiply(_Rphiz);
 
-  const axesPoints: Matrix[] = [
-    new Matrix(4, 1, [[5], [0], [0], [0]]),
-    new Matrix(4, 1, [[0], [0], [0], [0]]),
-    new Matrix(4, 1, [[0], [5], [0], [0]]),
-    new Matrix(4, 1, [[0], [0], [0], [0]]),
-    new Matrix(4, 1, [[0], [0], [5], [0]]),
-    new Matrix(4, 1, [[0], [0], [0], [0]]),
-  ];
+  const drawGridxy = (
+    context: CanvasRenderingContext2D,
+    rotateMatrix: Matrix
+  ) => {
+    for (let i = -5; i <= 5; i++) {
+      let _px = new Matrix(4, 1, [[i], [-5], [0], [0]]);
+      let px = new Matrix(4, 1, [[i], [5], [0], [0]]);
+      let _py = new Matrix(4, 1, [[-5], [i], [0], [0]]);
+      let py = new Matrix(4, 1, [[5], [i], [0], [0]]);
 
+      _px = rotateMatrix.multiply(_px);
+      px = rotateMatrix.multiply(px);
+      _py = rotateMatrix.multiply(_py);
+      py = rotateMatrix.multiply(py);
+
+      drawLinexyAxes(context, _px, px);
+      drawLinexyAxes(context, _py, py);
+    }
+  };
+
+  const drawGridxz = (
+    context: CanvasRenderingContext2D,
+    rotateMatrix: Matrix
+  ) => {
+    for (let i = -5; i <= 5; i++) {
+      let _px = new Matrix(4, 1, [[i], [0], [-5], [0]]);
+      let px = new Matrix(4, 1, [[i], [0], [5], [0]]);
+      let _pz = new Matrix(4, 1, [[-5], [0], [i], [0]]);
+      let pz = new Matrix(4, 1, [[5], [0], [i], [0]]);
+
+      _px = rotateMatrix.multiply(_px);
+      px = rotateMatrix.multiply(px);
+      _pz = rotateMatrix.multiply(_pz);
+      pz = rotateMatrix.multiply(pz);
+
+      drawLinexyAxes(context, _px, px);
+      drawLinexyAxes(context, _pz, pz);
+    }
+  };
+
+  const drawGridyz = (
+    context: CanvasRenderingContext2D,
+    rotateMatrix: Matrix
+  ) => {
+    for (let i = -5; i <= 5; i++) {
+      let _py = new Matrix(4, 1, [[0], [i], [-5], [0]]);
+      let py = new Matrix(4, 1, [[0], [i], [5], [0]]);
+      let _pz = new Matrix(4, 1, [[0], [-5], [i], [0]]);
+      let pz = new Matrix(4, 1, [[0], [5], [i], [0]]);
+
+      _py = rotateMatrix.multiply(_py);
+      py = rotateMatrix.multiply(py);
+      _pz = rotateMatrix.multiply(_pz);
+      pz = rotateMatrix.multiply(pz);
+
+      drawLinexyAxes(context, _py, py);
+      drawLinexyAxes(context, _pz, pz);
+    }
+  };
+
+  const drawAxes = (
+    context: CanvasRenderingContext2D,
+    rotateMatrix: Matrix
+  ) => {
+    const axesPoints: Matrix[] = [
+      new Matrix(4, 1, [[0], [0], [0], [0]]),
+      new Matrix(4, 1, [[5], [0], [0], [0]]),
+      new Matrix(4, 1, [[0], [5], [0], [0]]),
+      new Matrix(4, 1, [[0], [0], [5], [0]]),
+      new Matrix(4, 1, [[-5], [0], [0], [0]]),
+      new Matrix(4, 1, [[0], [-5], [0], [0]]),
+      new Matrix(4, 1, [[0], [0], [-5], [0]]),
+    ];
+
+    const poligon = axesPoints.map((value) => {
+      return rotateMatrix.multiply(value);
+    });
+
+    context.beginPath();
+    context.moveTo(
+      poligon[1].at(0, 0) * scale + context.canvas.width / 2,
+      context.canvas.height / 2 - poligon[1].at(1, 0) * scale
+    );
+    context.lineTo(
+      poligon[1].at(0, 0) * scale + context.canvas.width / 2 - 15,
+      context.canvas.height / 2 - 15 - poligon[1].at(1, 0) * scale
+    );
+    context.lineTo(
+      poligon[1].at(0, 0) * scale + context.canvas.width / 2 - 15,
+      context.canvas.height / 2 + 15 - poligon[1].at(1, 0) * scale
+    );
+    context.fill();
+
+    for (let i = 1; i < axesPoints.length; i++) {
+      drawLinexyAxes(context, axesPoints[0], poligon[i]);
+    }
+    for (let i = -5; i <= 5; i++) {
+      let l = new Matrix(4, 1, [[i], [0], [0], [0]]);
+      l = rotateMatrix.multiply(l);
+      context.beginPath();
+      context.font = "bold 15px sans-serif";
+      context.fillText(
+        String(i),
+        Number(l.at(0, 0)) * scale + context.canvas.width / 2,
+        context.canvas.height / 2 - l.at(1, 0) * scale
+      );
+    }
+    for (let i = -5; i <= 5; i++) {
+      let l = new Matrix(4, 1, [[0], [i], [0], [0]]);
+      l = rotateMatrix.multiply(l);
+      context.beginPath();
+      context.font = "bold 15px sans-serif";
+      context.fillText(
+        String(i),
+        Number(l.at(0, 0)) * scale + context.canvas.width / 2,
+        context.canvas.height / 2 - l.at(1, 0) * scale
+      );
+    }
+    for (let i = -5; i <= 5; i++) {
+      let l = new Matrix(4, 1, [[0], [0], [i], [0]]);
+      l = rotateMatrix.multiply(l);
+      context.beginPath();
+      context.font = "bold 15px sans-serif";
+      context.fillText(
+        String(i),
+        Number(l.at(0, 0)) * scale + context.canvas.width / 2,
+        context.canvas.height / 2 - l.at(1, 0) * scale
+      );
+    }
+  };
   //console.log(coswy, coswz, sinwz, phix, phiy);
 
   const scale = 40;
@@ -110,85 +220,56 @@ function Workspace({ k1, k2, k3 }: WorkspaceInfo) {
   const draw = (context: CanvasRenderingContext2D) => {
     //console.log(kxold, kyold, kzold, kx, ky, kz);
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    const poligon = rotatePoints(points);
+    const poligon = rotatePoints(points, R);
     poligon.forEach((point, i) => {
       drawLinexy(context, point, poligon[(i + 1) % poligon.length]);
     });
-    const axes = rotatePoints(axesPoints);
+    /*const axes = rotatePoints(axesPoints, R);
     axes.forEach((point, i) => {
       drawLinexy(context, point, axes[(i + 1) % axes.length]);
-    });
+    });*/
+    drawGridxy(context, R);
+    drawGridxz(context, R);
+    drawGridyz(context, R);
+    drawAxes(context, R);
     setKxold(kx), setKyold(ky), setKzold(kz);
   };
 
   const drawFront = (context: CanvasRenderingContext2D) => {
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    const poligon = rotatePointsFront(points);
+    const poligon = rotatePoints(points, rotateMatrixXY);
     poligon.forEach((point, i) => {
       drawLinexy(context, point, poligon[(i + 1) % poligon.length]);
     });
-    const axes = rotatePointsFront(axesPoints);
-    axes.forEach((point, i) => {
-      drawLinexy(context, point, axes[(i + 1) % axes.length]);
-    });
+    drawGridxy(context, rotateMatrixXY);
+    drawAxes(context, rotateMatrixXY);
   };
 
   const drawUp = (context: CanvasRenderingContext2D) => {
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    const poligon = rotatePointsUp(points);
+    const poligon = rotatePoints(points, rotateMatrixXZ);
     poligon.forEach((point, i) => {
-      drawLinexz(context, point, poligon[(i + 1) % poligon.length]);
+      drawLinexy(context, point, poligon[(i + 1) % poligon.length]);
     });
-    const axes = rotatePointsUp(axesPoints);
-    axes.forEach((point, i) => {
-      drawLinexz(context, point, axes[(i + 1) % axes.length]);
-    });
+    drawGridxz(context, rotateMatrixXZ);
+    drawAxes(context, rotateMatrixXZ);
   };
 
   const drawRight = (context: CanvasRenderingContext2D) => {
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    const poligon = rotatePointsRight(points);
+    const poligon = rotatePoints(points, rotateMatrixYZ);
     poligon.forEach((point, i) => {
-      drawLineyz(context, point, poligon[(i + 1) % poligon.length]);
+      drawLinexy(context, point, poligon[(i + 1) % poligon.length]);
     });
-    const axes = rotatePointsRight(axesPoints);
-    axes.forEach((point, i) => {
-      drawLineyz(context, point, axes[(i + 1) % axes.length]);
-    });
+    drawGridyz(context, rotateMatrixYZ);
+    drawAxes(context, rotateMatrixYZ);
   };
 
-  const rotatePoints = (points: Matrix[]) => {
+  const rotatePoints = (points: Matrix[], rotateMatrix: Matrix) => {
     const figure = points;
 
     const poligon = figure.map((value) => {
-      return R.multiply(value);
-    });
-    return poligon;
-  };
-
-  const rotatePointsUp = (points: Matrix[]) => {
-    const figure = points;
-
-    const poligon = figure.map((value) => {
-      return rotateMatrixXZ.multiply(value);
-    });
-    return poligon;
-  };
-
-  const rotatePointsFront = (points: Matrix[]) => {
-    const figure = points;
-
-    const poligon = figure.map((value) => {
-      return rotateMatrixXY.multiply(value);
-    });
-    return poligon;
-  };
-
-  const rotatePointsRight = (points: Matrix[]) => {
-    const figure = points;
-
-    const poligon = figure.map((value) => {
-      return rotateMatrixYZ.multiply(value);
+      return rotateMatrix.multiply(value);
     });
     return poligon;
   };
@@ -204,37 +285,25 @@ function Workspace({ k1, k2, k3 }: WorkspaceInfo) {
     const dispXp2 = p2.at(0, 0) * scale + context.canvas.width / 2;
     const dispYp2 = context.canvas.height / 2 - p2.at(1, 0) * scale;
     context.beginPath();
+    context.strokeStyle = "red";
+    context.lineWidth = 4;
     context.moveTo(dispXp1, dispYp1);
     context.lineTo(dispXp2, dispYp2);
     context.stroke();
+    context.lineWidth = 1;
+    context.strokeStyle = "black";
   };
 
-  const drawLinexz = (
+  const drawLinexyAxes = (
     context: CanvasRenderingContext2D,
     p1: Matrix,
     p2: Matrix
   ) => {
     if (p1 == null || p2 == null) return;
     const dispXp1 = p1.at(0, 0) * scale + context.canvas.width / 2;
-    const dispYp1 = context.canvas.height / 2 - p1.at(2, 0) * scale;
+    const dispYp1 = context.canvas.height / 2 - p1.at(1, 0) * scale;
     const dispXp2 = p2.at(0, 0) * scale + context.canvas.width / 2;
-    const dispYp2 = context.canvas.height / 2 - p2.at(2, 0) * scale;
-    context.beginPath();
-    context.moveTo(dispXp1, dispYp1);
-    context.lineTo(dispXp2, dispYp2);
-    context.stroke();
-  };
-
-  const drawLineyz = (
-    context: CanvasRenderingContext2D,
-    p1: Matrix,
-    p2: Matrix
-  ) => {
-    if (p1 == null || p2 == null) return;
-    const dispXp1 = p1.at(1, 0) * scale + context.canvas.width / 2;
-    const dispYp1 = context.canvas.height / 2 - p1.at(2, 0) * scale;
-    const dispXp2 = p2.at(1, 0) * scale + context.canvas.width / 2;
-    const dispYp2 = context.canvas.height / 2 - p2.at(2, 0) * scale;
+    const dispYp2 = context.canvas.height / 2 - p2.at(1, 0) * scale;
     context.beginPath();
     context.moveTo(dispXp1, dispYp1);
     context.lineTo(dispXp2, dispYp2);
@@ -242,19 +311,11 @@ function Workspace({ k1, k2, k3 }: WorkspaceInfo) {
   };
 
   const points: Matrix[] = [
-    /*new Matrix(4, 1, [[1], [1], [1], [0]]),
-    new Matrix(4, 1, [[1], [1], [0], [0]]),
-    new Matrix(4, 1, [[0], [1], [0], [0]]),
+    new Matrix(4, 1, [[0], [0], [0], [0]]),
+    new Matrix(4, 1, [[0], [0], [1], [0]]),
     new Matrix(4, 1, [[0], [1], [1], [0]]),
-    new Matrix(4, 1, [[0], [0], [1], [0]]),
+    new Matrix(4, 1, [[0], [1], [0], [0]]),
     new Matrix(4, 1, [[0], [0], [0], [0]]),
-    new Matrix(4, 1, [[1], [0], [0], [0]]),
-    new Matrix(4, 1, [[1], [0], [1], [0]]),*/
-    new Matrix(4, 1, [[0], [0], [0], [0]]),
-    new Matrix(4, 1, [[0], [0], [1], [0]]),
-    new Matrix(4, 1, [[1], [0], [0], [0]]),
-    new Matrix(4, 1, [[0], [0], [0], [0]]),
-    new Matrix(4, 1, [[1], [2], [2], [0]]),
     new Matrix(4, 1, [[1], [0], [0], [0]]),
     new Matrix(4, 1, [[1], [2], [2], [0]]),
     new Matrix(4, 1, [[0], [0], [1], [0]]),
@@ -263,7 +324,13 @@ function Workspace({ k1, k2, k3 }: WorkspaceInfo) {
   const readPoints = (event) => {
     const input: HTMLInputElement = document.querySelector("#enterFile")!;
     const file = input.files![0];
-    console.log(file.name);
+
+    const reader = new FileReader();
+    reader.readAsText(file);
+
+    reader.onload = () => {
+      console.log(reader.result);
+    };
   };
   return (
     <div className="workspace">
@@ -274,6 +341,27 @@ function Workspace({ k1, k2, k3 }: WorkspaceInfo) {
       <div>
         <input id="enterFile" type="file" onChange={readPoints}></input>
         <input type="number"></input>
+        <table className="matrix">
+          <tr>
+            {R.at(0, 0).toFixed(4)} | {R.at(1, 0).toFixed(4)} |{" "}
+            {R.at(2, 0).toFixed(4)} | {R.at(3, 0).toFixed(4)}
+          </tr>
+          <tr>----------------------------------------------</tr>
+          <tr>
+            {R.at(0, 1).toFixed(4)} | {R.at(1, 1).toFixed(4)} |{" "}
+            {R.at(2, 1).toFixed(4)} | {R.at(3, 1).toFixed(4)}
+          </tr>
+          <tr>----------------------------------------------</tr>
+          <tr>
+            {R.at(0, 2).toFixed(4)} | {R.at(1, 2).toFixed(4)} |{" "}
+            {R.at(2, 2).toFixed(4)} | {R.at(3, 2).toFixed(4)}
+          </tr>
+          <tr>----------------------------------------------</tr>
+          <tr>
+            {R.at(0, 3).toFixed(4)} | {R.at(1, 3).toFixed(4)} |{" "}
+            {R.at(2, 3).toFixed(4)} | {R.at(3, 3).toFixed(4)}
+          </tr>
+        </table>
       </div>
     </div>
   );
